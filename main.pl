f64810a2de55a99d85c5b5dd95db017eeaaf6979
Atsuki Demizu <demmys.atsuki@gmail.com>
2015-01-19 03:33:46 +0900

    標準出力に情報を表示するよう変更

--- main.pl
+++ main.pl
@@ -29,6 +29,10 @@ my $api = TwitterAPI->new(
 # データベースの準備
 my $db = MonicoDB->new($conf->{db}->{path});
 
+# エラー格納用変数
+my $err;
+
+print "初期設定完了\n";
 
 # 新しいメンションを取得
 my $tweetID = $db->select_last_mention->{tweet_id};
@@ -37,6 +41,8 @@ foreach my $tweet ($api->mentions($tweetID)) {
     my $userID = $tweet->{user}->{id};
     my $screenName = $tweet->{user}->{screen_name};
     my $tweetID = $tweet->{id};
+
+    print $screenName." からの新しいメンション「".$tweet->{text}."」をデータベースに保存\n";
 
     # メンションをデータベースに保存
     $db->insert_mention(
@@ -66,7 +72,11 @@ foreach my $tweet ($api->mentions($tweetID)) {
             $callTime,
             $tweetID
         );
-        $api->update("@".$screenName." ".$conf->{line}->{set}->[int(rand(2))]);
+        print $screenName." を起こす設定を追加\n";
+        $err = $api->update("@".$screenName." ".$conf->{line}->{set}->[int(rand(2))]);
+        if($err) {
+            print "ツイート失敗: $err\n";
+        }
     }
 
     # モーニングコールの解除
@@ -76,20 +86,33 @@ foreach my $tweet ($api->mentions($tweetID)) {
         my @stoppings = $db->select_calls_between($from, $to, $userID);
         if (@stoppings > 0) {
             foreach my $s (@stoppings) {
+                print $s->{screen_name}." は".($s->{call_count})."回のアラートで起床\n";
                 my $name = "@".$s->{screen_name}." ";
                 my $pat = int(rand(2));
                 switch ($s->{call_count}) {
                     case 0 {
-                        $api->update($name.$conf->{line}->{bouquet}->{before}->[$pat]);
+                        $err = $api->update($name.$conf->{line}->{bouquet}->{before}->[$pat]);
+                        if($err) {
+                            print "ツイート失敗: $err\n";
+                        }
                     }
                     case 1 {
-                        $api->update($name.$conf->{line}->{bouquet}->{first}->[$pat]);
+                        $err = $api->update($name.$conf->{line}->{bouquet}->{first}->[$pat]);
+                        if($err) {
+                            print "ツイート失敗: $err\n";
+                        }
                     }
                     case 2 {
-                        $api->update($name.$conf->{line}->{bouquet}->{second}->[$pat]);
+                        $err = $api->update($name.$conf->{line}->{bouquet}->{second}->[$pat]);
+                        if($err) {
+                            print "ツイート失敗: $err\n";
+                        }
                     }
                     else {
-                        $api->update($name.$conf->{line}->{bouquet}->{last}->[$pat]);
+                        $err = $api->update($name.$conf->{line}->{bouquet}->{last}->[$pat]);
+                        if($err) {
+                            print "ツイート失敗: $err\n";
+                        }
                     }
                 }
                 $db->delete_call($s->{id});
@@ -105,10 +128,14 @@ my $max = DateTime->now(time_zone => 'local');
 for (my $i = 0; $i < $callLimit; $i++) {
     my @alerts = $db->select_calls_before($max, $i);
     foreach my $alert (@alerts) {
-        $api->update(
+        print $alert->{screen_name}." への".($alert->{call_count} + 1)."回目のアラート\n";
+        $err = $api->update(
             "@".$alert->{screen_name}." ".
             $conf->{line}->{alert}->[$alert->{call_count} % 3]->[int(rand(2))]
         );
+        if($err) {
+            print "ツイート失敗: $err\n";
+        }
         $db->increment_call_count($alert->{id});
     }
     $max->subtract(minutes => $callTime / $callLimit);
@@ -126,8 +153,14 @@ foreach my $failure (@failures) {
         }
     }
     my $line = "@".$failure->{screen_name}." ".$conf->{line}->{decrial};
+    print $failure->{screen_name}." は起きなかったため、";
     foreach my $target (@targets) {
         $line .= ".".$friends->[$target]->{screen_name}." ";
+        print $friends->[$target]->{screen_name}." ";
     }
-    $api->update($line);
+    print "にそのことを通知\n";
+    $err = $api->update($line);
+    if($err) {
+        print "ツイート失敗: $err\n";
+    }
 }
